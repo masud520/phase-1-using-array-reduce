934f8dcfd69b443c933a435013212c8d71d5b86d
masud520 <masud.abdi@student.moringaschool.com>
2023-10-18 10:42:24 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+
+let totalBatteries = 0;
+function reducer(batteryBatches) {
+   
+    return batteryBatches.reduce(function(totalBatteries, batch){return totalBatteries + batch, 0});
+    if (typeof assembly === "number") {
+        totalBatteries += assembly;
+    }
+  }
+ 
